0f1a400e5800b9b79cbd17b5fac49b01c94572af
Replit user <>
2022-11-14 02:32:48 +0000

    data structure and login implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from "./views/loginPage";
+import GestionPage from "./views/gestionPage";
+import NewEventPage from "./views/newEventPage";
+import FormPage from "./views/formPage";
+import PageNotFound from "./views/pageNotFound";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import AuthContext from './components/authProvider';
+import { useContext } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const { auth } = useContext(AuthContext);
+    
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="*" exact element={<PageNotFound />} />
+                <Route path="/" element={auth ? <GestionPage /> : <Navigate replace to="/login" />} />
+                <Route path="/newEvent" element={auth ? <NewEventPage /> : <Navigate replace to="/login" />} />
+                <Route path="/form" element={auth ? <FormPage /> : <Navigate replace to="/login" />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/views/formPage.js
@@ -0,0 +1,65 @@
+import AuthContext  from '../components/authProvider'
+import { useContext } from 'react'
+import '../css/form.css'
+
+export default function FormPage() {
+    const { auth } = useContext(AuthContext)
+    console.log(auth)
+  
+    const items = {
+      "fname": ["Nombre", "text"],
+      "lname": ["Apellidos", "text"],
+      "genre": ["Género", "text"],
+      "address2": ["Cidudad", "text"],
+      "birthdate": ["Fecha de Nacimiento", "date"],
+      "eaddress": ["Correo electrónico", "email"],
+      "phone": ["Teléfono celular", "tel"],
+      "extrdata": ["Información adicional", "extradata"]
+    }
+    const questions = ["Teléfono", "Correo", "Cualquiera"]
+
+    const sendData = async (e) => {
+        e.preventDefault()
+    }
+  
+    return(
+        <div className="testbox">
+            <form onSubmit={sendData}>
+                <div className="banner"><h1>Pre-inscripción al evento</h1></div>
+                <div className="colums">
+                    {Object.keys(items).map((key, index) => {
+                        const [name, type] = items[key]
+                        return (
+                            <div key={index} className="item">
+                                <label htmlFor={key}>{name}<span>*</span></label>
+                                <input id={key} type={type} name={key} required />
+                            </div>
+                        )
+                    })}
+                </div>
+        
+                <div className="question">
+                    <label>¿Por donde prefieres ser contactado? </label>
+                    <div className="question-answer">
+                        {questions.map((question, index) => {
+                            const name = 'radio'
+                            const element = `${name}_${index + 4}`
+                            return (
+                                <div key={index}>
+                                    <input type={name} value="none" id={name} name="contact"/>
+                                    <label htmlFor={element} className={name} ><span>{question}</span></label>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <h2>Términos y condiciones</h2>
+                <input type="checkbox" name="checkbox1" />
+                <label>Nos asegura que los datos que nos acaba de suministrar son fieles a la realidad que corresponde, y así mismo consiente que esta información sea almacenada en una base de datos y usada para la identificación dentro del evento con fines administrativos.</label>
+                <div className="btn-block">
+                    <button type="submit">Enviar datos</button>
+                </div>
+            </form>
+        </div>
+    )
+}
