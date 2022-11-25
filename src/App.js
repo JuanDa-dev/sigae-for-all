1b704836a28f7901cadcb4a150472552ad3c7467
MT1120 <mateoanaya6@gmail.com>
2022-11-25 14:27:18 -0500

    Login and Sign Up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import LoginPage from "./views/loginPage";
+import RegisterPage from "./views/RegisterPage"
 import GestionPage from "./views/gestionPage";
 import FormPage from "./views/formPage";
 import PageNotFound from "./views/pageNotFound";
@@ -14,6 +15,7 @@ export default function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/Register" element={<RegisterPage />} />
                 <Route path="/" element={auth ? <GestionPage /> : <Navigate replace to="/login" />} />
                 <Route path="/form" element={auth ? <FormPage /> : <Navigate replace to="/login" />} />
                 <Route path="*" exact element={<PageNotFound />} />
--- src/views/loginPage.js
+++ src/views/loginPage.js
@@ -29,33 +29,40 @@ export default function LoginPage() {
     }
 
     return (
-        <div className="container">
-            <div className="screen">
-                <div className="screen__content">
-                    <form className="login" onSubmit={sendData} >
-                        <div className="login__field">
-                            <i className="login__icon fas fa-user"></i>
-                            <input type="text" className="login__input" onChange={e => {
+        
+
+        <div className='Login_Container'>
+
+            <form className='Form_Login' onSubmit={sendData}>
+                
+                <div className='input_Container'>
+
+                    <i className="login__icon fas fa-user"></i>
+                    <input type= "text" className='inputLogin' onChange={e => {
                                 setUsername(e.target.value)
                             }} placeholder="Nombre de usuario" />
-                        </div>
-                        <div className="login__field">
-                            <i className="login__icon fas fa-lock"></i>
-                            <input type="password" className="login__input" onChange={e => {
+                </div>
+                <div className='input_Container'>
+
+                    <i className="login__icon fas fa-lock"></i>
+                    <input type= "password" className='inputLogin' onChange={e => {
                                 setPassword(e.target.value)
                             }} placeholder="Contraseña" />
-                        </div>
-                        <button type="submit" className="button login__submit">
-                            <span className="button__text">Ingresa</span>
-                            <i className="button__icon fas fa-chevron-right"></i>
-                        </button>				
-                    </form>
                 </div>
-                <div className="screen__background">
-                    <span className="screen__background__shape screen__background__shape3"></span>		
-                    <span className="screen__background__shape screen__background__shape1"></span>
-                </div>		
-            </div>
+                <button type="submit" className="button login__submit">
+                        <span className="button__text">Ingresa</span>
+                        <i className="button__icon fas fa-chevron-right"></i>
+                </button>
+                <div className='Sign_Up'>
+
+                    <div>
+                        No tienes un Usuario
+                        <a href='www.cursos.uninorte.edu.co'> Registrate aqui</a>
+                    </div>
+                    
+
+                </div>
+            </form>
         </div>  
     )
 }
