49e3b1eb940a57d34e83a6d7a00adc506a4a727f
Edimar-star <edimarod@outlook.es>
2022-11-27 22:45:19 -0500

    form implemented

--- src/views/formPage.js
+++ src/views/formPage.js
@@ -1,17 +1,33 @@
-import AuthContext  from '../components/authProvider'
-import { useContext } from 'react'
+import { postRequest, getRequest } from '../components/requests';
+import { useParams } from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid';
+import { useState } from 'react';
 import '../css/form.css'
 
 export default function FormPage() {
-    const { auth } = useContext(AuthContext)
-  
+    const { form_id } = useParams();
+    const [attendee, setAttendee] = useState({
+        name: "",
+        lastName: "",
+        gender: "",
+        email: "",
+        role: "",
+        birthdate: "",
+        city: "",
+        cellphone: "",
+        info: "",
+        event_id: "",
+        qrcode_description: "",
+        qrcode_id: ""
+    })
     const items = {
       "fname": ["Nombre", "text"],
       "lname": ["Apellidos", "text"],
       "genre": ["Género", "text"],
-      "address2": ["Cidudad", "text"],
-      "birthdate": ["Fecha de Nacimiento", "date"],
       "eaddress": ["Correo electrónico", "email"],
+      "role": ["Rol", "rol"],
+      "birthdate": ["Fecha de Nacimiento", "date"],
+      "address2": ["Ciudad", "text"],
       "phone": ["Teléfono celular", "tel"],
       "extrdata": ["Información adicional", "extradata"]
     }
@@ -19,19 +35,36 @@ export default function FormPage() {
 
     const sendData = async (e) => {
         e.preventDefault()
+        attendee.birthdate = new Date(attendee.birthdate)
+        const qrcode_id = uuidV4()
+        const event_id = (await getRequest({ 
+            name: `data_events/event_by_form/${form_id}`,
+        })).id
+        attendee.qrcode_description = `${process.env.REACT_APP_URL}/api/data_events/attendees/${qrcode_id}`
+        attendee.qrcode_id = qrcode_id
+        setAttendee(attendee)
+        const response = await postRequest({ 
+            name: `data_events/attendees/${event_id}`,
+            payload: attendee
+        })
+        console.log(response)
     }
   
     return(
         <div className="testbox">
             <form onSubmit={sendData}>
-                <div className="banner"><h1>Pre-inscripción al evento</h1></div>
+                <div className="banner"><h1>Inscripción al evento</h1></div>
                 <div className="colums">
                     {Object.keys(items).map((key, index) => {
                         const [name, type] = items[key]
+                        const value = Object.keys(attendee)[Object.keys(items).indexOf(key)]
                         return (
                             <div key={index} className="item">
                                 <label htmlFor={key}>{name}<span>*</span></label>
-                                <input id={key} type={type} name={key} required />
+                                <input id={key} type={type} onChange={e => {
+                                    attendee[value] = e.target.value;
+                                    setAttendee(attendee)
+                                }} name={key} required />
                             </div>
                         )
                     })}
@@ -45,7 +78,7 @@ export default function FormPage() {
                             const element = `${name}_${index + 4}`
                             return (
                                 <div key={index}>
-                                    <input type={name} value="none" id={name} name="contact"/>
+                                    <input type={name} value={question} id={name} name="contact"/>
                                     <label htmlFor={element} className={name} ><span>{question}</span></label>
                                 </div>
                             )
